13698600664994eb4dde6b0581dc7ec0174a9350
denzilferreira <ferreiradenzil@gmail.com>
2017-02-20 22:22:18 +0200

    - Remove Aware.startPlugin call.

--- com.aware.plugin.studentlife.audio/src/main/java/com/aware/plugin/studentlife/audio_final/Plugin.java
+++ com.aware.plugin.studentlife.audio/src/main/java/com/aware/plugin/studentlife/audio_final/Plugin.java
@@ -67,9 +67,6 @@ public class Plugin extends Aware_Plugin {
         DATABASE_TABLES = Provider.DATABASE_TABLES;
         TABLES_FIELDS = Provider.TABLES_FIELDS;
         CONTEXT_URIS = new Uri[]{Provider.StudentLifeAudio_Data.CONTENT_URI};
-
-        //Activate plugin
-        Aware.startPlugin(this, PLUGIN_NAME);
     }
 
     //This function gets called every 5 minutes by AWARE to make sure this plugin is still running.
