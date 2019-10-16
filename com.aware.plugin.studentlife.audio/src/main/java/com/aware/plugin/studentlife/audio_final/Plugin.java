258dac12e837927080a3250e9cee959a79168f55
Denzil Ferreira <ferreiradenzil@gmail.com>
2019-10-16 15:40:13 +0300

    - Fix plugins' automatically starting - Fixes to parsing of server URLs - Support HTTP deployments

--- com.aware.plugin.studentlife.audio/src/main/java/com/aware/plugin/studentlife/audio_final/Plugin.java
+++ com.aware.plugin.studentlife.audio/src/main/java/com/aware/plugin/studentlife/audio_final/Plugin.java
@@ -68,8 +68,14 @@ public class Plugin extends Aware_Plugin {
             //Check if the user has toggled the debug messages
             DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
 
-            //Initialize our plugin's settings
-            Aware.setSetting(this, Settings.STATUS_PLUGIN_STUDENTLIFE_AUDIO, true);
+            if (Aware.getSetting(getApplicationContext(), Settings.STATUS_PLUGIN_STUDENTLIFE_AUDIO).length() == 0) {
+                Aware.setSetting(getApplicationContext(), Settings.STATUS_PLUGIN_STUDENTLIFE_AUDIO, true);
+            } else {
+                if (Aware.getSetting(getApplicationContext(), Settings.STATUS_PLUGIN_STUDENTLIFE_AUDIO).equalsIgnoreCase("false")) {
+                    Aware.stopPlugin(getApplicationContext(), getPackageName());
+                    return START_STICKY;
+                }
+            }
 
             if (Aware.getSetting(this, Settings.PLUGIN_CONVERSATIONS_DELAY).length() == 0)
                 Aware.setSetting(this, Settings.PLUGIN_CONVERSATIONS_DELAY, 1);
